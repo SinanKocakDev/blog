74722f0c53936b2f8ce856fc53330e486b5ab938
Sinan <sinankocakdev@gmail.com>
2024-07-31 04:51:06 +0300

    Yerel değişikliklerinizi açıklayan bir mesaj

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ import db from "./config/dbConnect.js"
 import postRoutes from "./routes/postRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import cors from "cors"
+import helmet from "helmet"
 import path from "path"
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
@@ -12,22 +18,19 @@ dotenv.config()
 db()
 
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api/posts', postRoutes)
 app.use('/api/auth', userRoutes)
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+  );
+
 const port = process.env.PORT || 3001
-
-
-if(process.env.NODE_ENV === "production") {
-    const dirPath = path.resolve()
-    app.use(express.static(path.join("client/dist")))
-    app.get("*",(req,res) => {
-        res.sendFile(path.resolve(dirPath,"client","dist","index.html"))
-    })
-}
-
 
 app.listen(port, () => {
     console.log(`Server is started on ${port} port`);
